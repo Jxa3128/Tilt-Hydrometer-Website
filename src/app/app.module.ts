c4afdd6bc095111399dee21a39a5e1dbc4208ea7
Jorge Avila Juarez <jorgejuarez@Jorges-MBP.attlocal.net>
2021-07-17 14:01:19 -0500

    added firebaseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,17 @@ import { AppComponent } from './app.component';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 
-export const firebaseConfig={
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtPSHEXPkqfrP5F42_kjMSCiH6b5rGGgQ",
+  authDomain: "senior-design-tilt.firebaseapp.com",
+  databaseURL: "https://senior-design-tilt-default-rtdb.firebaseio.com",
   projectId: "senior-design-tilt",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-}
+  storageBucket: "senior-design-tilt.appspot.com",
+  messagingSenderId: "564989379674",
+  appId: "1:564989379674:web:4ef854b8aae1e31655c463",
+  measurementId: "G-J1LG6Y23LS"
+};
 @NgModule({
   declarations: [
     AppComponent
