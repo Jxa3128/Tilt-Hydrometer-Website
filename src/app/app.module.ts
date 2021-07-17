c1dc927fdb5574399760af1c2d5cc6b51a52a34d
Jorge Avila Juarez <jorgejuarez@Jorges-MBP.attlocal.net>
2021-07-17 13:55:05 -0500

    added dependencies + firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+
+export const firebaseConfig={
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "senior-design-tilt",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
